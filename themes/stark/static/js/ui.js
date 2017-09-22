957d52c8a30301dd181e6dcbd00dc07d7771eae9
Niko Kovacevic <nikovacevic@gmail.com>
2017-09-21 20:31:39 -0600

    Nav updates

--- themes/stark/static/js/ui.js
+++ themes/stark/static/js/ui.js
@@ -1,25 +1,15 @@
 if (!UI) {
   var UI = {};
 }
-
 (function () {
-  // Functions
-
-  // toggleMenu toggles display of dropdown list elements in the given menu
-  UI.toggleMenu = function (menu) {
-    var lis = menu.getElementsByClassName("dropdown-li");
-    Array.prototype.forEach.call(lis, function (li) {
-      li.classList.toggle("hidden");
-      //li.style.display = li.style.display == "none" ? "block" : "none";
-    });
-  }
-
-  // Events
-
-  // On click of dropdown button, toggle dropdown menu display
-  var btn = document.getElementById("dropdown-button");
-  var menu = document.getElementById("dropdown-menu");
-  btn.onclick = function() {
-    UI.toggleMenu(menu);
+  // On click of menu button, toggle menu
+  var btn = document.getElementById("nav-button");
+  btn.onclick = function () {
+    var headerNav = document.getElementById("header-nav");
+    var primaryNav = document.getElementById("primary-nav");
+    var secondaryNav = document.getElementById("secondary-nav");
+    primaryNav.classList.toggle("hidden-sm");
+    secondaryNav.classList.toggle("hidden-sm");
+    headerNav.classList.toggle("secondary");
   };
 })();
